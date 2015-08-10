efaa4cb3c838abb258ec574c5af75eeb27ec10a6
Jonathan Dahan <jonathan@jonathan.is>
2015-08-10 17:02:27 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
-fn main() { return; }
+extern crate rand;
+use rand::Rng;
+
+fn main() { 
+   let unsorted = vec![ 5, 15, 20, 13 ];
+   println!("{:?}", unsorted);
+   let sorted = merge_sort(unsorted);
+   println!("{:?}", sorted);
+   return;
+}
 
 #[test]
 fn test_merge_sort() {
-  let unsorted = vec![ 5, 15, 20, 13 ];
-  let sorted = selection_sort(unsorted);
+  let mut unsorted: Vec<usize> = vec![];
+  let mut rng = rand::thread_rng();
+  for times in 1..99 {
+    unsorted.push(rng.gen::<usize>());
+  }
+  let sorted = merge_sort(unsorted);
 
-  assert_eq!(sorted, [5, 13, 15, 20]);
+  assert_eq!(unsorted.sort(), sorted);
 }
 
 #[test]
 fn test_selection_sort() {
   let unsorted = vec![ 5, 15, 20, 13 ];
-  let sorted = merge_sort(unsorted);
+  let sorted = selection_sort(unsorted);
 
   assert_eq!(sorted, [5, 13, 15, 20]);
 }
@@ -31,11 +44,14 @@ fn selection_sort<T: Ord+Clone>(numbers: Vec<T>) -> Vec<T> {
   return numbers;
 }
 
-fn merge_sort<T: Ord+Clone>(mut arr: Vec<T>) -> Vec<T> {
-  if arr.len()<2 { return arr };
+fn merge_sort(mut arr: Vec<usize>) -> Vec<usize> {
+  if arr.len()<2 { 
+      return arr };
   if arr.len()==2 {
-      if arr[0] > arr[1] { arr.swap(0,1); }
-      return arr;
+    if arr[0] > arr[1] { 
+      arr.swap(0,1);
+    }
+    return arr;
   }
 
   let (left, right) = arr.split_at(arr.len()/2);
@@ -45,17 +61,22 @@ fn merge_sort<T: Ord+Clone>(mut arr: Vec<T>) -> Vec<T> {
   );
 }
 
-fn merge<T: Ord+Clone>(left: Vec<T>, right: Vec<T>) -> Vec<T>{
+fn merge(left: Vec<usize>, right: Vec<usize>) -> Vec<usize>{
   let mut merged = vec![];
-  let mut j=0;
-  let mut k=0;
-  while merged.len() < left.len()+right.len() {
-    if left[j] < right[k] {
-        merged.push(left[j].clone());
-        j=j+1;
+  let mut left = left.clone();
+  left.push(std::usize::MAX);
+  let mut right = right.clone();
+  right.push(std::usize::MAX);
+  let mut l=0;
+  let mut r=0;
+  while merged.len() < left.len()-1+right.len()-1 {
+    println!("merged is {:?}", merged);
+    if left[l] < right[r] {
+        merged.push(left[l].clone());
+        l=l+1;
     } else {
-        merged.push(right[k].clone());
-        k=k+1;
+        merged.push(right[r].clone());
+        r=r+1;
     }
   }
   return merged;
